a49a0605a423f3ffdc2a56f62da3b9b8b9d14ba4
Dinis Vieira <dinisvieira@outlook.com>
2015-05-04 00:22:50 +0100

    commit of Windows Track code (complete)

---
+++ Code/WindowsDev/XamarinMemeGenerator/XamarinMemeGenerator/XamarinMemeGenerator.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace XamarinMemeGenerator.WinPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            //Calls the Shared Portable Class Library to get a list with all available meme's.
+            ObservableCollection<string> memes = await WantSomeMemesNowClass.ShowMeThoseMemes();
+
+            //Set the list of memes to our ComboBox and enable it
+            MemesListView.ItemsSource = memes;
+            MemesListView.IsEnabled = true;
+        }
+
+        private async void GenerateMyMemeBtn_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (MemesListView.SelectedValue == null) { return; } //make sure we have a selected value
+
+            //Calls the Shared Portable Class Library with the values of the ComboBox and TextBox's in this View.
+            //The returned value is the image in a byte array format 
+            byte[] imageBytes = await WantSomeMemesNowClass.GenerateMyMeme(MemesListView.SelectedValue.ToString(), TopTextBox.Text, BottomTextBox.Text);
+
+            //Create Image
+            var bitmapImage = new BitmapImage();
+            var stream = new InMemoryRandomAccessStream();
+            await stream.WriteAsync(imageBytes.AsBuffer());
+            stream.Seek(0);
+            bitmapImage.SetSource(stream);
+            
+            //Set image to the Image Placeholder we have on our View
+            Image.Source = bitmapImage;
+        }
+    }
+}
